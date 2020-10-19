5738ee0aaf3cddcf24f4011e5a397c57a6d13642
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2020-10-19 18:21:50 -0500

    created variables color1, color2, color3, color 4. Assigned each variable linear-gradient value. Created array variable colorsArray with each of the color variables as its values. Ran console.log(colorsArray).

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ bodY.appendChild(wrapper);
 
 document.getElementsByTagName('div')[0].classList.add('wrapper');
 
-//Repeat to create four 'div' elements //
+// Repeat to create four 'div' elements //
 // created new variable 'divs' and div element//
 // 4 divs are nested within wrapper variable //
 // added class name 'wrapperDivs' to divs variable//
@@ -23,3 +23,11 @@ for (var i = 0; i < 4; i ++) {
     divs.classList = 'wrapperDivs';
     wrapper.appendChild(divs);
 }
+
+// Creating variables: color1, color2, color3, color4 //
+var color1 = "linear-gradient(135deg, #0B132B 0%, #3A506B 100%)";
+var color2 = "linear-gradient(330deg, #5BC0BE 0%, #3A506B 100%)";
+var color3 = "linear-gradient(35deg, #0B132B 0%, #5BC0BE 100%)";
+var color4 = "linear-gradient(235deg, #5BC0BE 0%, #3A506B 100%)";
+
+var colorsArray = ["color1", "color2", "color3", "color4"];
