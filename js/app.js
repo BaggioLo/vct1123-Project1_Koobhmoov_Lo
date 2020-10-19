7509a80283afafa0d183370646bb4b17dc6c6130
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2020-10-19 01:38:00 -0500

    used the setAttribute method to pass in "rel", "stylesheet" as arguments

--- js/app.js
+++ js/app.js
@@ -3,3 +3,6 @@ var addToHead = document.querySelector("head");
 var cssLink = document.createElement("link");
 
 addToHead.appendChild(cssLink);
+
+document.getElementsByTagName("link").setAttribute("rel", "stylesheet");
+
