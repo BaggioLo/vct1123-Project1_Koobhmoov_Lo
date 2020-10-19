762a3cf16fe14b5e3793358dc51aa70235bb4ea1
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2020-10-19 17:56:51 -0500

    Used "for" loop to create 4 'div' elements nested inside of element wrapper class. Created new variable 'divs' and used createElement method to create 'div' lement. Used appendChild method on wrapper variable to add divs to the DOM. Used the classList method to add class name 'wrapperDivs' to divs variable.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
+// Creating link within head and setting attributes //
 var addToHead = document.querySelector('head');
-
 var cssLink = document.createElement('link');
-
 addToHead.appendChild(cssLink);
 
 document.getElementsByTagName('link')[1].setAttribute('rel', 'stylesheet');
@@ -15,3 +14,9 @@ bodY.appendChild(wrapper);
 
 document.getElementsByTagName('div')[0].classList.add('wrapper');
 
+//Repeat to create four 'div' elements //
+for (var i = 0; i < 4; i ++) {
+    var divs = document.createElement("div");
+    divs.classList = 'wrapperDivs';
+    wrapper.appendChild(divs);
+}
