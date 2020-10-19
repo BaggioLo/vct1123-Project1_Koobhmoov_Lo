c0906c558fcfda6b0ed77f1962ad56c05fd2641e
BaggioLo <72361665+BaggioLo@users.noreply.github.com>
2020-10-19 14:47:24 -0500

    Launched HTML page in Chrome and Developer tools. Link element is nested in the head element,  div has class of "wrapper" nested within the body 'element'.

--- js/app.js
+++ js/app.js
@@ -4,15 +4,14 @@ var cssLink = document.createElement('link');
 
 addToHead.appendChild(cssLink);
 
-document.getElementsByTagName('link').setAttribute('rel', 'stylesheet');
+document.getElementsByTagName('link')[1].setAttribute('rel', 'stylesheet');
 
-document.getElementsByTagName('link').setAttribute('href', 'css/style.css');
+document.getElementsByTagName('link')[1].setAttribute('href', 'css/style.css');
 
-var wrapper = document.createElement('div');
-
-document.getElementsByTagName('div').classList.add('wrapper');
-
+// Added div and wrapper //
 var bodY = document.querySelector('body');
-
+var wrapper = document.createElement('div');
 bodY.appendChild(wrapper);
+
+document.getElementsByTagName('div')[0].classList.add('wrapper');
 
